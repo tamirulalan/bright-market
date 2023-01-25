61ac12d8a66674cf86d2db15d3e5720fb35da61c
tamirulalan <tamirulalan@gmail.com>
2023-01-25 12:28:16 -0500

    Added cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './display/HomeScreen';
+import ProductScreen from './display/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge';
+import { useContext } from 'react';
+import { Store } from './Store';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="info" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Bright Market</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
-}
-
-export default App;
+    }
+export default App
--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+// import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { StoreProvider } from './Store';
+// import { StoreProvider } from './Store';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-  
+    
+    <StoreProvider>
       <HelmetProvider>
-        <PayPalScriptProvider deferLoading={true}>
-          <App />
-        </PayPalScriptProvider>
+        <App />
       </HelmetProvider>
+    </StoreProvider>
    
   </React.StrictMode>
 );
